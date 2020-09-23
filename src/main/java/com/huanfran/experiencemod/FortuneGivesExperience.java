12af03b3defa933f19eceb06725fc3f161459891
HuanFran <53756287+HuanFran@users.noreply.github.com>
2020-09-23 13:05:22 +1000

    Change files from 1.16 to 1.12
    Initial Commit

--- src/main/java/com/huanfran/experiencemod/FortuneGivesExperience.java
+++ src/main/java/com/huanfran/experiencemod/FortuneGivesExperience.java
@@ -1,23 +1,48 @@
 package com.huanfran.experiencemod;
 
-import com.huanfran.experiencemod.config.Config;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.init.Enchantments;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-/**
- * A mod that allows the fortune and looting enchantments to grant additional experience to the player when mining and
- * killing mobs.
- */
-@Mod(FortuneGivesExperience.MODID)
+@Mod(modid = "fortune-gives-experience", name = "Fortune Gives Experience", version = "1.0.0")
+@Config(modid = "fortune-gives-experience")
+@Mod.EventBusSubscriber(modid = "fortune-gives-experience")
 public class FortuneGivesExperience {
 
 
-    public static final String MODID = "fortune-gives-experience";
+
+    @Config.Comment("The xp scalar for a tool's fortune level. 0.5 by default.")
+    public static double fortuneScalar = 0.5;
+
+    @Config.Comment("The xp scalar for a weapon's looting level. 0.5 by default.")
+    public static double lootingScalar = 0.5;
 
 
-    public FortuneGivesExperience() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT_SPEC);
+
+
+    @SubscribeEvent
+    public static void onBlockBroken(BlockEvent.BreakEvent event) {
+        if(event.getPlayer() == null) return;
+
+        int fortune = EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, event.getPlayer().getHeldItemMainhand());
+
+        if(fortune > 0) event.setExpToDrop((int) Math.ceil(event.getExpToDrop() * (1 + fortune * fortuneScalar)));
+    }
+
+
+
+
+    @SubscribeEvent
+    public static void onLivingExperienceDrop(LivingExperienceDropEvent event) {
+        if(event.getAttackingPlayer() == null) return;
+
+        int looting = EnchantmentHelper.getEnchantmentLevel(Enchantments.LOOTING, event.getAttackingPlayer().getHeldItemMainhand());
+
+        if(looting > 0) event.setDroppedExperience((int) Math.ceil(event.getDroppedExperience() * (1 + looting * lootingScalar)));
     }
 
 
